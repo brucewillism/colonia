c54b223ca451dc9bb3d1cfcd987d6ef6f8f78bf4
brucewillism <bruce.willis.br07@gmail.com>
2018-06-19 19:10:11 -0300

    atualizações

--- index.php
+++ index.php
@@ -1,41 +1,15 @@
 <?php
 include 'php/menu.php';
-
 ?>
-<br>
-<br>
-<br>
+<?php
+echo "Exorcizamus te, omnis immundus spiritus,
+omnis satanica potestas, omnis incuriso infernalis adversarii,
+omnis legio, omnis congredatio et secta diabolica…
+Ergo…
+Perditionis venenum propinare. Vade, satana,
+inventor et magister omnis fallaciae. Hostis humanae salutis.
+Humiliare sub potenti manu dei. Contremisce et effuge.
+Invocato a nobis sancto et terribile nomine. Quem inferi tremunt…
+Ab insidis diaboli, libera nos, domine. Ut ecclesiam tuam secura tibi facias,
 
-<form action="pesquisar.php" method="POST" id='form-contato' class="form-horiz
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="css/itens.css">
-    <link rel="stylesheet"  href="recursos/font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="shortcut icon" href="imagem/Stokage.png" type="image/x-png">
-    <title>Stockager</title>
-</head>
-<body>
-     <?php
-
-        require_once 'bd/conexao.php';
-
-        $stmt = $conn->query('SELECT * FROM cadastro ');
-        $cadastro = $stmt->fetchAll();
-        foreach ($cadastro as $dados) {
-
-        $id = $dados['id'];
-        $nome=$dados['nome'];
-        $quantidade = $dados['quantidade'];
-        $preco = $dados['preco'];
-        $categoria = $dados['nome_cat'];
-        echo
-        <td><a href='../bd/excluir.php?id=".$id."'><i class='fa fa-2x fa-trash sr-icons' title='Deletar Produto'></i></a></td>
-        <td><a href='formulario_editar.php?id=".$id."'><i class='fa fa-2x fa-pencil sr-icons' title='Editar Produto'></i></a></td>
-
-        </tr>;
-        }
-?>
-
-</body>
-</html>
+libertate servire, te rogamus, audi nos.;" ?>
