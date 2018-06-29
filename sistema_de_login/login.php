f70f6d7bc4c06927342d0e5df3d4c59a8bb10b41
brucewillism <bruce.willis.br07@gmail.com>
2018-06-28 23:06:09 -0300

    atualizações

--- sistema_de_login/login.php
+++ sistema_de_login/login.php
@@ -6,15 +6,6 @@ include '../menu_lateral.php';
 
 	<title>Login</title>
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
@@ -49,21 +40,19 @@ include '../menu_lateral.php';
 						</label>
 					</div>
  -->
-					<div class="container-login100-form-btn">
+
+
+				<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
 							Login
 						</button>
 
 					</div>
-
-
-
-<!-- 					<div class="text-center p-t-90">
-						<a class="txt1" href="#">
-							Forgot Password?
-						</a>
+ 				</form>
+ 				<form class="login100-form validate-form" action="../sistema_de_cadastro_usuario/cadastro_usuario.php" method="POST">
+					<div class="container-login100-form-btn">
+				   	<button class="login100-form-btn" type="submit"> Cadastre-se!</button>
 					</div>
- -->				</form>
 			</div>
 		</div>
 	</div>
@@ -71,19 +60,5 @@ include '../menu_lateral.php';
 
 	<div id="dropDownSelect1"></div>
 
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-	<script src="vendor/animsition/js/animsition.min.js"></script>
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-	<script src="vendor/select2/select2.min.js"></script>
-	<script src="vendor/daterangepicker/moment.min.js"></script>
-	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-	<script src="vendor/countdowntime/countdowntime.js"></script>
-	<script src="js/main.js"></script>
-
-	<form class="login100-form validate-form" action="../sistema_de_cadastro_usuario/cadastro_usuario.php" method="POST">
-    	Ainda não é cadastrado?
-    	<button class="login100-form-btn" type="submit"> Cadastre-se! </button>
-    </form>
 </body>
 </html>
