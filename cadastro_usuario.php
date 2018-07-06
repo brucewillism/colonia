448d92e24dd38f8d1fc235ff85a9f4b2ec68f5ec
lucielly <luciellyx5@gmail.com>
2018-07-05 22:53:39 -0300

    layout do login e cadastro

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -7,61 +7,39 @@ include"cabeçalho.php"
 <head>
 	<title>Cadastra-se</title>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="css/util1.css">
-	<link rel="stylesheet" type="text/css" href="css/main1.css">
-
+<link rel="stylesheet" type="text/css" href="css/util1.css">
+<link rel="stylesheet" type="text/css" href="css/main1.css">
+<link rel="stylesheet" type="text/css" href="css/blog-home.css">
 </head>
 <body>
+<form class="login100-form validate-form" action="bd/cadastrarUsuario.php" method="POST">   
+     <div class="login-box">
+    <img src="img/1avatar.png" class="avatar">
+        <h1>Cadastre-se</h1>
+            <form>
+            <p>Nome </p>
+            <input type="te" name="login" placeholder="Seu Nome Completo">
+			<p>Nome de Usuário</p>
+            <input type="uso" name="usuario" placeholder="Seu Login">
+			<p>Email </p>
+            <input type="em" name="Email" placeholder="Seu email">
+            <p>Senha</p>
+            <input type="password" name="senha" placeholder="Sua Senha">
 
-	<div class="limiter">
-		<div class="container-login100">
-			<div class="login100-more"></div>
 
-			<div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
-				<form class="login100-form validate-form" action="bd/cadastrarUsuario.php" method="POST">
-					<span class="login100-form-title p-b-59">
-						Inscrever-se
-					</span>
 
-					<div class="wrap-input100 validate-input" data-validate="Name is required">
-						<span class="label-input100">Nome completo</span>
-						<input class="input100" type="text" name="nome" placeholder="Name...">
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<span class="label-input100">Email</span>
-						<input class="input100" type="email" name="email" placeholder="Email addess...">
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate="Username is required">
-						<span class="label-input100">Nome de usuário</span>
-						<input class="input100" type="text" name="login" placeholder="Username...">
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate = "Password is required">
-						<span class="label-input100">Senha</span>
-						<input class="input100" type="password" name="senha" placeholder="Digite sua Senha">
-						<span class="focus-input100"></span>
-					</div>
-
-					<!-- <div class="wrap-input100 validate-input" data-validate = "Repeat Password is required">
-						<span class="label-input100">Repita a senha</span>
-						<input class="input100" type="text" name="repeat-pass" placeholder="*************">
-						<span class="focus-input100"></span>
-					</div>
-				--><div class="container-login100-form-btn">
-					<input class="login100-form-btn" type="submit" value="Salvar">
-
-				</div>
-			</form>
-		</div>
-	</div>
+            <input type="submit" name="submit" value="Login">
+            <a href="#">Esqueçeu sua Senha?</a>    
+            </form>
+        
+        
+        </div>
 </div>
-<?php
-include "rodape.php"
-?>
+	</div>
+		</div>
+			
+			</div>
+
+
 </body>
 </html>
--- login.php
+++ login.php
@@ -4,51 +4,30 @@ include"cabeçalho.php";
 <title>Login</title>
 <link rel="stylesheet" type="text/css" href="css/util.css">
 <link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="blog-home.css">
 </head>
 <body>
-
-	<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100">
-				<form class="login100-form validate-form" action="login_autenticação.php" method="POST">
-<!-- 					<span class="login100-form-logo">
-						<i class="zmdi zmdi-landscape"></i>
-					</span>
-				-->
-				<span class="login100-form-title p-b-34 p-t-27">
-					Entrar
-				</span>
-
-				<div class="wrap-input100 validate-input">
-					<input class="input100" type="text" name="login" placeholder="Nome do Usuario">
-				</div>
-
-				<div class="wrap-input100 validate-input">
-					<input class="input100" type="password" name="senha" placeholder="Senha">
-				</div>
-
-					<!-- <div class="contact100-form-checkbox">
-						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-						<label class="label-checkbox100" for="ckb1">
-							Remember me
-						</label>
-					</div>
-				-->
-
-
-				<div class="container-login100-form-btn">
-					<button class="login100-form-btn">
-						Login
-					</button>
-
-				</div>
-			</form>
-		</div>
-	</div>
+<form class="login100-form validate-form" action="login_autenticação.php" method="POST">
+    <div class="logi-box">
+    <img src="img/1avatar.png" class="avatar">
+        <h1>Entrar</h1>
+            <form>
+            <p>Nome</p>
+            <input type="te" name="login" placeholder="Seu Login">
+            <p>Senha</p>
+            <input type="password" name="senha" placeholder="Sua Senha">
+            <input type="submit" name="submit" value="Login">
+            <a href="#">Esqueçeu sua Senha</a>    
+            </form>
+        
+        
+        </div>
+    
 </div>
-<?php
-include "rodape.php"
-?>
+	</div>
+		</div>
+			
+			</div>
 
 </body>
 </html>
